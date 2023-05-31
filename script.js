0392a66c287a4b0c61ab4faf91fc8fef8f31f563
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-05-31 13:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
-//your code here
 class OutOfRangeError extends Error {
   constructor() {
-    super('Expression should only consist of integers and +-/* characters and not <arg>');
-    this.name = 'OutOfRangeError';
+    super();
+    this.name = this.constructor.name;
+    this.message = 'Expression should only consist of integers and +-/* characters';
   }
 }
 
 class InvalidExprError extends Error {
   constructor() {
-    super('Expression should not have an invalid combination of operators');
-    this.name = 'InvalidExprError';
+    super();
+    this.name = this.constructor.name;
+    this.message = 'Expression should not have an invalid combination of expression';
   }
 }
 
 function evalString(expression) {
   try {
-    if (expression.match(/[^\d\s+\-*/]/g)) {
+    if (/[\d\s\+\-\*\/]+/.test(expression)) {
+      if (/^\+|\*|\//.test(expression)) {
+        throw new SyntaxError('Expression should not start with invalid operator');
+      }
+      
+      if (/[\+\-\*\/]$/.test(expression)) {
+        throw new SyntaxError('Expression should not end with invalid operator');
+      }
+      
+      if (/[+\-*/]{2}/.test(expression)) {
+        throw new InvalidExprError();
+      }
+    } else {
       throw new OutOfRangeError();
     }
-
-    if (expression.match(/\+\+|--|\+\-|\-\+|\*\+|\/\+/)) {
-      throw new InvalidExprError();
-    }
-
-    if (expression.startsWith('+') || expression.startsWith('/') || expression.startsWith('*')) {
-      throw new SyntaxError('Expression should not start with an invalid operator');
-    }
-
-    if (expression.endsWith('+') || expression.endsWith('/') || expression.endsWith('*') || expression.endsWith('-')) {
-      throw new SyntaxError('Expression should not end with an invalid operator');
-    }
-
-    // Evaluate the expression here (not implemented in this example)
+    
     return eval(expression);
   } catch (error) {
-    if (error instanceof OutOfRangeError || error instanceof InvalidExprError) {
-      console.error(`Error: ${error.message}`);
-    } else {
+    if (error instanceof OutOfRangeError || error instanceof InvalidExprError || error instanceof SyntaxError) {
       throw error;
+    } else {
+      throw new Error('An error occurred while evaluating the expression');
     }
+  }
+}
+
+function evaluateExpression() {
+  const expressionInput = document.getElementById('expressionInput');
+  const result = document.getElementById('result');
+  const error = document.getElementById('error');
+
+  try {
+    const expression = expressionInput.value;
+    const evaluatedResult = evalString(expression);
+    result.textContent = `Result: ${evaluatedResult}`;
+    error.textContent = '';
+  } catch (error) {
+    error.textContent = error.message;
+    result.textContent = '';
   }
 }
